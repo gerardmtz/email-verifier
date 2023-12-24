5bab0fe87123b5a0b33c2872f1e50d260adcb3f8
gerardmtz <gerartmartinez@gmail.com>
2023-12-23 18:53:17 -0600

    - Files and folders organized - Starting the Frontend development for the app.

---
+++ email-verifier/client/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "log"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+    app := fiber.New();
+
+    app.Get("/", func ( c *fiber.Ctx ) error {
+
+        return c.SendString("Hello From email-verifier's Frontend :D")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
